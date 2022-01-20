c8b713cfb6c4884b6be955d852d29d1c2c49e534
JeongKyunPark <park32323@gmail.com>
2022-01-20 14:22:49 +0900

    Modify preprocessing.py
    - change arguments

--- preprocessing.py
+++ preprocessing.py
@@ -46,8 +46,9 @@ def target_to_sentence(target, id2char):
 
 
 
-def generate_character_script(videos_paths, audios_paths, transcripts,mode):
+def generate_character_script(videos_paths, audios_paths, transcripts,test=False):
     print('create_script started..')
+    mode = 'Test' if test else 'Train'
     if mode == 'Train':
         char2id, id2char = load_label("./dataset/labels.csv")
     
@@ -66,17 +67,21 @@ def generate_character_script(videos_paths, audios_paths, transcripts,mode):
 
 
     
-def preprocess(dataset_path,mode):
+def preprocess(dataset_path, test=False):
     print('preprocess started..')
+    mode = 'Test' if test else 'Train'
     transcripts=[]
-    dataset_path_video = data_folder + '/Video/*'
+    
+    dataset_path_video = data_folder + '/Video_npy/*'
     videos_paths = glob.glob(dataset_path_video)
     videos_paths = sorted(videos_paths)
 
     dataset_path_audio = data_folder + '/Audio/*.wav'
     audios_paths = glob.glob(dataset_path_audio)
     audios_paths = sorted(audios_paths)
-    if mode == 'Train':
+    
+    
+    if mode=='Train':
         for file_ in tqdm(audios_paths):
             txt_file_ = file_.replace('.wav','.txt')
             txt_file_ = txt_file_.replace('Audio','Text')
@@ -92,8 +97,8 @@ def preprocess(dataset_path,mode):
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_folder', type=str, required=True)
-    parser.add_argument('--mode', type=str, required=True)
+    parser.add_argument('-d', '--data_folder', type=str, required=True)
+    parser.add_argument('-t', '--test', action='store_true')
     args = parser.parse_args()
     return args
 
@@ -101,6 +106,6 @@ if __name__ == '__main__':
     
     args = get_args()
     data_folder = args.data_folder
-    mode = args.mode
-    videos_paths, audios_paths, transcripts = preprocess(data_folder,mode)
-    generate_character_script(videos_paths,audios_paths, transcripts, mode)
+    test = args.test
+    videos_paths, audios_paths, transcripts = preprocess(data_folder, test=test)
+    generate_character_script(videos_paths,audios_paths, transcripts, test=test)
